0ffddb600dc15721ce6031377e98033ca4b3895a
jkiddd <ivan_x_@mail.ru>
2019-04-04 21:53:16 +0600

    в консоль лог

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 var  one = 'Моя первая переменная';
 var myclass = 'вывод спомьщью класса'
 // alert(one);
-console.log(one);
+// console.log(one);
 
 // document.getElementById('message').innerHTML = one;
 
 // вывод спощью джейквери по  ID
-$('#message').text(one);
+// $('#message').text(one);
 
 // анимация джейквери
 $('#message').fadeOut('slow');
 // вывод спомощью классов
 $('.myclass').text(one);
 // вывод спомощью JS
-document.getElementsByClassName("myclass")[0].innerHTML =  one;
+// document.getElementsByClassName("myclass")[0].innerHTML =  one;
+console.log(document.getElementsByClassName("myclass"));
