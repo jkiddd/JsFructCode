fbf041500eb9aa9a0930bbe22aa6948e637bfe4f
jkiddd <ivan_x_@mail.ru>
2019-04-04 21:47:08 +0600

    вывод спомощью классов

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var  one = 'Моя первая переменная';
-
+var myclass = 'вывод спомьщью класса'
 // alert(one);
 console.log(one);
 
 // document.getElementById('message').innerHTML = one;
+
 // вывод спощью джейквери по  ID
-// $('#message').text(one);
+$('#message').text(one);
+
+// анимация джейквери
+$('#message').fadeOut('slow');
+// вывод спомощью классов
+$('.myclass').text(one);
+// вывод спомощью JS
+document.getElementsByClassName("myclass")[0].innerHTML =  one;
