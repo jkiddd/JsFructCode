0023804f5385206455c295453e5fd21c6b264ef1
jkiddd <ivan_x_@mail.ru>
2019-04-06 12:42:02 +0600

    массив

--- js/main.js
+++ js/main.js
@@ -22,3 +22,23 @@ $('.myclass').css('background-color','black');
 $('.myclass').css('border','2px solid green');
 $('.myclass').css('margin','15px 20px');
 $('.myclass').css('padding','20px');
+
+
+var arrayString = ['Первый', 'Второй', 'Третий', 'Четвертый', 'Пятый'];
+
+/*
+*   Функция reverse() "переворачивает" массив arrayString.
+*/
+arrayString = arrayString.reverse();
+
+/*
+*   Выводим в цикле все элементы массива arrayString
+*   Обратите внимание на метод jQuery append. В отличии от метода text,
+*   данный метод добавляет в HTML новые значения, а старые не удаляет. 
+*  
+*   Задание: Примените функцию toUpperCase() в цикле ниже, чтобы все
+*   элементы массива стали в верхнем регистре
+*/
+for(var i = 0; i < arrayString.length; i++) {
+    $('#result').append(arrayString[i].toUpperCase() + '<br>');
+}
